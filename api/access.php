4ee44f4f191e97b97740e2cfb7df5807c9bd59f9
Luis Ricardo Sanchez <lsanchez@esferasoluciones.com>
2019-03-20 14:33:17 -0500

    listo

--- api/access.php
+++ api/access.php
@@ -3,17 +3,22 @@ include "helper.php";
 
 //$post = retrieveJsonPostData($_POST);
 
-if(isset($_POST['u']) && isset($_POST['p']) && $_POST['u'].length && $_POST['p'].length) {
+if(isset($_POST['u']) && isset($_POST['p']) && isset($_POST['t']) && $_POST['u'].length && $_POST['p'].length && $_POST['t'].length) {
     $username = $_POST['u'];
     $password = $_POST['p'];
 
     $user = R::findOne( 'users', ' email = ?', [ $username ] );
     if($user) {
         if($user->password == $password){
-            echo json_encode(array('status' => 'ok', 'user'=>$user));
+            if($user->user_type_id == $_POST['t']){
+                echo json_encode(array('status' => 'ok', 'user'=>$user));
 
-            $user->last_login_date = date("Y-m-d H:i:s");
-            $id = R::store( $user );
+                $user->last_login_date = date("Y-m-d H:i:s");
+                $id = R::store( $user );
+            }
+            else{
+                echo json_encode(array('status' => 'fail', 'msg'=>'no platform'));
+            }
         }
         else
             echo json_encode(array('status' => 'fail', 'msg'=>'no password'));
